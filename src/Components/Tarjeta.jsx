434434bdcf29bf4443636914898cca26f759372f
Freddy Soto <orlandes259@gmail.com>
2024-06-20 11:55:59 -0400

    componente tarjeta

---
+++ src/Components/Tarjeta.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Tarjeta.css"
+import Texto from "./Texto"
+// import imagen from './public/Ejer'
+const Tarjeta = () => {
+  return (
+
+     <div className="contenedor">
+      
+        <Texto/>   
+        <img src="./EjercicioB.png" alt="" />
+    </div>
+  )
+}
+
+export default Tarjeta
---
+++ src/Components/Texto.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Texto.jsx"
+const Texto = () => {
+  return (
+    <div className='Texto'>
+        <div>Puxada Frontal</div>
+        <div>3 séries x 12 repetições</div>
+    </div>
+  )
+}
+
+export default Texto
